4a2cbc4cc67ecaac9b01d1835705023016f08ed4
Aansh Kotian <76598799+Aone52@users.noreply.github.com>
2023-07-08 18:15:08 -0400

    changed amplify user pool

---
+++ client/runnersden/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "runnersden59deab8859deab88": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
